3faf9a6e685c8360840bee8c5578a76577a29917
Jan Marcek <jan.marcek@eea.sk>
2014-11-03 10:18:13 +0100

    setup.py was created, project reference to nevcommons was updated

---
+++ setup.py
@@ -0,0 +1,29 @@
+'''
+@author: jmc
+'''
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(
+    name='ckancommons',
+    version=version,
+    description="""
+    Ckan's commons for development
+    """,
+    long_description="""
+    """,
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='Jan Marcek',
+    author_email='jmc@eea.sk',
+    url='',
+    license='',
+    packages=find_packages(exclude=['examples', 'tests']),
+    namespace_packages=['ckancommons'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        ''
+    ],
+)
