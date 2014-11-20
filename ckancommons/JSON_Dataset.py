d1658885a682c86d53322bad5c2ec15247bab0b9
Jan Marcek <jan.marcek@eea.sk>
2014-11-20 09:08:19 +0100

    added descrtiption into ckan's pacakges

--- ckancommons/JSON_Dataset.py
+++ ckancommons/JSON_Dataset.py
@@ -52,16 +52,17 @@ class JSON_Dataset():
         # license is creative commons by default 
         self.license = 'cc-by'
         self.owner_org = []
+        self.description = ''
 
 
     def tostring(self):
-        return   u"[%s] name: [%s] title: [%s] notes: [%s] author: [%s] extras: [%s] resource: [%s] owner_org:[%s]" % (self.__class__.__name__, self.name, self.title , self.notes , self.author, self.extras, self.resources, self.owner_org )
+        return   u"[%s] name: [%s] title: [%s] notes: [%s] author: [%s] extras: [%s] resource: [%s] owner_org:[%s]" % (self.__class__.__name__, self.name, self.title , self.notes , self.author, self.extras, self.resources, self.owner_org)
 
     def tojson_without_resource(self):
-        return { "name" : self.name, "title" :  self.title, "notes": self.notes, "author": self.author, "extras": self.extras , "resources" : [], "license_id" : self.license, "owner_org" : self.owner_org}
+        return { "name" : self.name, "title" :  self.title, "notes": self.notes, "author": self.author, "extras": self.extras , "resources" : [], "license_id" : self.license, "owner_org" : self.owner_org, "notes": self.description}
 
     def tojson_all(self):
-        return { "name" : self.name, "title" :  self.title, "notes": self.notes, "author": self.author, "extras": self.extras , "license_id"  : self.license, "resources": self.resources, "owner_org" : self.owner_org}
+        return { "name" : self.name, "title" :  self.title, "notes": self.notes, "author": self.author, "extras": self.extras , "license_id"  : self.license, "resources": self.resources, "owner_org" : self.owner_org, "notes": self.description}
 
     def tojson_resource(self):
         if len(self.resources) > 0:
